9e0c93b0c882f42a6864fe3857adb111c1ed1ffb
Aquib Momin <aquibmomin007@gmail.com>
2020-08-20 21:02:02 +0800

    react app cli initiated

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App';
+import DropDown from './components/DropDown/DropDown';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="main-container">
+        <DropDown />
+      </div>
+    </div>
+  );
+}
+
+export default App;
